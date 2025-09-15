56b5eb5884a704aca3f7b0290212440390d44351
root <ilnazletsplay@gmail.com>
2025-09-15 13:28:21 +0000

    fix

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -9,7 +9,9 @@ window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 axios.defaults.withCredentials = true
+
 axios.defaults.baseURL = '/';
+
 
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
