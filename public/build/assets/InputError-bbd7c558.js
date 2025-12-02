8986a8ab4771f3e60155dc1319a42b72bb5f0838
miki3074 <miki23074@gmail.com>
2025-12-02 19:27:00 +0300

    Add pravki2vu

---
+++ public/build/assets/InputError-bbd7c558.js
@@ -0,0 +1,1 @@
+import{j as t,ad as s,o as a,f as r,b as o,t as c}from"./app-fb5242ae.js";const n={class:"text-sm text-red-600 dark:text-red-400"},d={__name:"InputError",props:{message:{type:String}},setup(e){return(i,m)=>t((a(),r("div",null,[o("p",n,c(e.message),1)],512)),[[s,e.message]])}};export{d as _};
