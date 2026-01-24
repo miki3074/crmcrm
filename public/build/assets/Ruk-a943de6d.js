06440ab588b350f73761fb6a7be9c8ec095d0c5b
miki3074 <miki23074@gmail.com>
2026-01-24 14:56:58 +0300

    Add new24vu

--- public/build/assets/Ruk-a943de6d.js
+++
@@ -1,1 +0,0 @@
-import{o as t,c as r,k as e,u as a,F as o,Z as s}from"./app-0c17e5ad.js";import{_ as c}from"./AuthenticatedLayout-e4f4bca7.js";import"./ApplicationLogo-792ed34e.js";import"./_plugin-vue_export-helper-c27b6911.js";const f={__name:"Ruk",setup(m){return(n,_)=>(t(),r(o,null,[e(a(s),{title:"Руководство "}),e(c)],64))}};export{f as default};
