07dddeb2727daa87506467e744f566c5bb4007f2
miki3074 <miki23074@gmail.com>
2026-04-24 13:55:27 +0300

    Add TelegramBinding component and build assets

---
+++ public/build/assets/Ruk-918b2cdb.js
@@ -0,0 +1,1 @@
+import{o as t,a,q as e,u as r,F as o,Z as s}from"./app-bdb3a8f5.js";import{_ as m}from"./AuthenticatedLayout-d6b78e10.js";import"./ApplicationLogo-0aad146b.js";import"./_plugin-vue_export-helper-c27b6911.js";const f={__name:"Ruk",setup(n){return(_,c)=>(t(),a(o,null,[e(r(s),{title:"Руководство "}),e(m)],64))}};export{f as default};
