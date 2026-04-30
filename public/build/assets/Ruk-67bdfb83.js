60bfed1d5e81395feec20af433a3e98750f6e936
miki3074 <miki23074@gmail.com>
2026-04-30 06:15:21 +0300

    99vu

--- public/build/assets/Ruk-67bdfb83.js
+++
@@ -1,1 +0,0 @@
-import{o as t,a,q as e,u as r,F as o,Z as s}from"./app-f342c355.js";import{_ as m}from"./AuthenticatedLayout-49b5a0ec.js";import"./ApplicationLogo-a2baf064.js";import"./_plugin-vue_export-helper-c27b6911.js";const f={__name:"Ruk",setup(n){return(_,c)=>(t(),a(o,null,[e(r(s),{title:"Руководство "}),e(m)],64))}};export{f as default};
