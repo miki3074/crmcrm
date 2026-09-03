af37ca9af5e9e4da48c436cbb65c6bb6b5f2e44d
SjBg <miki23074@gmail.com>
2026-09-03 03:58:19 +0300

    izminenia

---
+++ public/build/assets/Ruk-4a40cff1.js
@@ -0,0 +1,1 @@
+import{o as a,a as t,e,u as r,F as o,bU as s}from"./app-19b81bec.js";import{_ as m}from"./AuthenticatedLayout-fc7bb53b.js";import"./ApplicationLogo-6c25c55b.js";import"./_plugin-vue_export-helper-c27b6911.js";const f={__name:"Ruk",setup(n){return(_,c)=>(a(),t(o,null,[e(r(s),{title:"Руководство "}),e(m)],64))}};export{f as default};
